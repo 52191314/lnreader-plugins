46cfe0dac1be8a28f1696898af298bc5ba8ad4fb
52191314 <xianspired@gmail.com>
2026-08-05 10:09:29 +0700

    fix(english): release version 1.0.1 with corrected lightnovelworld chapter-range filter

--- plugins/english/lightnovelworld.ts
+++ plugins/english/lightnovelworld.ts
@@ -23,7 +23,7 @@ export class LightNovelWorldPlugin implements Plugin.PluginBase {
   name = 'LightNovelWorld';
   icon = 'src/en/lightnovelworld/icon.png';
   site = 'https://lightnovelworld.org/';
-  version = '1.0.0';
+  version = '1.0.1';
 
   filters = {
     sort: {
