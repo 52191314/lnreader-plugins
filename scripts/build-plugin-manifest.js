0f53347e4db0529d07f4bc5e82595b2a6e88db96
52191314 <xianspired@gmail.com>
2026-08-16 13:20:07 +0700

    feat(english): release version 1.0.0 with Mynovels plugin

--- scripts/build-plugin-manifest.js
+++ scripts/build-plugin-manifest.js
@@ -7,10 +7,10 @@ import { minify } from './terser.js';
 const REMOTE = execSync('git remote get-url origin')
   .toString()
   .replace(/[\s\n]/g, '');
-const CURRENT_BRANCH = execSync('git branch --show-current')
-  .toString()
-  .replace(/[\s\n]/g, '');
-const BRANCH = process.env.BRANCH ? process.env.BRANCH : CURRENT_BRANCH;
+const CURRENT_BRANCH = (
+  process.env.BRANCH || execSync('git rev-parse --abbrev-ref HEAD').toString()
+).replace(/[\s\n]/g, '');
+const BRANCH = CURRENT_BRANCH;
 const matched = REMOTE.match(/([^:/]+?)\/([^/.]+)(\.git)?$/);
 if (!matched) throw Error('Cant parse git url');
 const USERNAME = matched[1];
